962ae6cde7da7bc1cf896e9cbc296aa85ee24aaa
bsk94 <annabiskup07@gmail.com>
2022-12-15 15:29:08 +0100

    react query installed and setup, query devtools added

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </React.StrictMode>
+    </QueryClientProvider>
   </Provider>
 );
